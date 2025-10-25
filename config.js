f2552339679c945c49aa861dbabfdf974a724af3
znbdev <znbdev@outlook.com>
2025-10-25 23:15:10 +0900

    feat(sso): 优化 SSO 和 JWT 相关模块，更新环境变量配置和页面逻辑

---
+++ config.js
@@ -0,0 +1,15 @@
+// config.js
+const SSO_CONFIG = {
+  clientId: `example_client_id`,
+  publicKey: `-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlOrRi+znKtOfpPQAZ6xc
+IsEgs6x3atEEP28n/GyVfn2QaejkYyDBTHiLKOUc3bzJ2rvAarPBcGjPy/SRgfAa
+1k2rpwtE78WNz8dc5O873TPdiq0yz2czziM8PPmvPkzGdSdiDbAitCLRz0hTqW7J
+Lv4qK3DxynX5/sxWTlk/QyyoB53yLqc15hLu2NrQJegS2luNfGAM/TUWzwIFTqao
+w6SdsvtVYgBZ+2QCIj4BhoskFX54RBWHGoHRQ/3UffY6fitrY9P0KvJpOWTLIyR9
+cI717rPZN8XLrp2P/cygmmiUlSOGx0flhxMuJubeCKEhWD7Og+OurQ8J0ZLAViTB
+MQIDAQAB
+-----END PUBLIC KEY-----`
+};
+
+module.exports = SSO_CONFIG;
